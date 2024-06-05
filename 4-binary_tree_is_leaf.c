1ff46cec38a002bfafbda7cebc1caf5d0c4d8019
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-06-05 12:14:01 -0700

    Fun for leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,7 +9,7 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	int leaf = 0;
 
-	if (node->left == NULL && node->right == NULL)
+	if (node && !(node->left) && !(node->right))
 		leaf = 1;
 
 	return (leaf);

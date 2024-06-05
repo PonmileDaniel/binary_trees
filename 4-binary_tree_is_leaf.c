11fd6c7290917bfffa7c4c7f7f23c0c2a0ef1f77
Ndukwe Daniel <pomiledaniel@gmail.com>
2024-06-05 12:11:14 -0700

    Func that check if a node is aleaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,8 +7,10 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->left == NULL && node->right == NULL)
-		return 1;
+	int leaf = 0;
 
-	return 0;
+	if (node->left == NULL && node->right == NULL)
+		leaf = 1;
+
+	return (leaf);
 }
